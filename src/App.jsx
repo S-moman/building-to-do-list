1abd64a0e6c976fd49cbd426c2ce92bd01b64cd2
Moe Moman <moemoman@moes-mac-mini.mynetworksettings.com>
2025-11-12 12:46:50 -0500

    recommit

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,46 @@ import "./App.css";
 import initialState from "./data";
 import { v4 as uuidv4 } from "uuid";
 import ToDoList from "./ToDoList";
-import ToDo from './ToDo'
-
+import ToDo from "./ToDo";
 
 function ToDoContainer() {
   const [todos, setToDos] = useState([]);
-  // const [todo, setToDo] = useState();
+  // const [todo, setToDo] = useState({});
 
   function addToDo(todo) {
-    if(todo !== ""){
-    setToDos([
-      ...todos,
-      { id: uuidv4(), title: todo, complete: false, isEditing: false },
-    ])};
+    if (todo !== "") {
+      setToDos([
+        ...todos,
+        { id: uuidv4(), title: todo, complete: false, isEditing: false },
+      ]);
+    }
   }
   // console.log(todos);
 
   function deleteToDo(id) {
-    setToDos(todos.filter(todo => todo.id !== id))
+    setToDos(todos.filter((todo) => todo.id !== id));
     console.log(`deleting task... - ${id}`);
   }
-// console.log(todos)
 
+  function handleEdit(id) {
+    setToDos(todos.map((todo) => todo.id === id){
+      ...todo, isEditing: !todo.isEditing
+    })
+    console.log("start editing...");
+  }
 
   return (
     <div className="container">
       <h1>To Do List</h1>
       <br />
-      <ToDoList addToDo={addToDo}/>
+      <ToDoList addToDo={addToDo} />
       {todos.map((todo, id) => (
-        <ToDo task={todo} key={id} deleteToDo={() => deleteToDo(todo.id)} />
+        <ToDo
+          task={todo}
+          key={id}
+          deleteToDo={() => deleteToDo(todo.id)}
+          handleEdit={handleEdit}
+        />
       ))}
     </div>
   );
--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
 
 export default function ToDo({ task, deleteToDo }) {
-  const [todo, setToDo] = useState({ task });
-
-  function handleEdit(task, id) {
-    console.log("start editing...");
-  }
-
- 
-
   return (
     <div className="toDoItem">
       <input type="checkbox" name="" id="task-checkbox" />
